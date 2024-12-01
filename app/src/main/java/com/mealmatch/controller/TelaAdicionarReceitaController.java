adbe1092bd929ef339810a5ca148361e19365747
Marcus César Santos Pereira <marcus.cesar.sp@gmail.com>
2024-11-30 23:51:11 -0300

    feat(tela_adicionar_receita): implementa a função de adicionar receitas no banco de dados
    adiciona a função de criação de receitas diretamente ligadas ao banco de dados com todas os seus
    campos e ligações como o de usuário dono da receita, junto a isso modifica o design da tela de
    adicionar receitas

--- app/src/main/java/com/mealmatch/controller/TelaAdicionarReceitaController.java
+++ app/src/main/java/com/mealmatch/controller/TelaAdicionarReceitaController.java
@@ -1,12 +1,23 @@
 package com.mealmatch.controller;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import com.mealmatch.jdbc.dao.IngredienteDAO;
+import com.mealmatch.jdbc.dao.ReceitaDAO;
+import com.mealmatch.jdbc.database.ConnectionFactory;
+import com.mealmatch.model.Ingrediente;
+import com.mealmatch.model.Receita;
+import com.mealmatch.utils.ControleDeSessao;
 import com.mealmatch.utils.ImageSelector;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -16,13 +27,15 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -31,10 +44,10 @@ public class TelaAdicionarReceitaController implements Initializable {
   @FXML
   private ImageView botaoVoltar;
 
-  @FXML 
+  @FXML
   private ImageView close_image;
 
-  @FXML 
+  @FXML
   private ImageView add_receita_image;
 
   @FXML
@@ -60,9 +73,6 @@ public class TelaAdicionarReceitaController implements Initializable {
 
   @FXML
   private ImageView receita_image;
-
-  @FXML
-  private Button tempoDePreparoButton;
 
   @FXML
   private TextField ingrediente_adicionado;
@@ -113,9 +123,8 @@ public class TelaAdicionarReceitaController implements Initializable {
   // Lista para armazenar o passo a passo do modo de preparo
   ObservableList<String> passosModoPreparo = FXCollections.observableArrayList();
 
-  private String receitaNome;
-
-  private float tempoDePreparo;
+  private byte[] imagemEmBytes;
+  List<Ingrediente> listaObjetoIngredientes;
 
   Scene scene;
   Stage stage;
@@ -123,24 +132,83 @@ public class TelaAdicionarReceitaController implements Initializable {
   @Override
   public void initialize(URL location, ResourceBundle resources) {
     unidadeDeMedida.getItems().addAll(unidadesDeMedida); // adicionando as unidades de medida dos ingredientes
-    listaView.setItems(listaIngredientes);
-    listaDePreparo.setItems(passosModoPreparo);
-    receitaNome = nomeDaReceita.getText();
-    System.out.println(receitaNome);
+    listaView.setItems(listaIngredientes);// setando a lista de ingredientes
+    listaDePreparo.setItems(passosModoPreparo);// setando a lista de preparo
 
-    // Cria um TextFormatter que permite apenas números inteiros
-    TextFormatter<Integer> formatter = new TextFormatter<>(change -> {
-      // Verifica se o novo texto é um número inteiro
-      if (change.getText().matches("/d*")) { // Apenas dígitos
-        return change;
-      }
-      return null; // Bloqueia a alteração caso não seja número
-    });
-
-    // Aplica o formatter no TextField
-    tempoDePrearoField.setTextFormatter(formatter);
+    listaObjetoIngredientes = new ArrayList<>();// inicializando a lista de objetos ingrediente
   }
 
+  @FXML
+  void criar_receita(MouseEvent event) throws IOException {
+    Receita receita = new Receita();
+    receita.setNome(nomeDaReceita.getText());
+    receita.setDificuldade(dificuldade);
+    receita.setModoPreparo(formatarModoPreparo());
+    receita.setTempoPreparo(formatarTempoPreparo());
+    receita.setImagem(new Image(new ByteArrayInputStream(imagemEmBytes)));
+    receita.setIdUsuarioDonoReceita(ControleDeSessao.getInstance().getUserId());
+    adicionarReceita(receita);
+    voltar_tela(event);
+  }
+
+  // Método para adicionar a receita no banco de dados
+  private void adicionarReceita(Receita receita) {
+
+    //Primeiro adiciona a receita no banco de dados junto com a relacao de usuario dono da receita
+    ReceitaDAO receitaDao = new ReceitaDAO(ConnectionFactory.getConnection());
+    int idReceita = receitaDao.adicionarReceitaCompleta(receita, imagemEmBytes,ControleDeSessao.getInstance().getUserId());
+
+    //Depois adiciona os ingredientes na receita por meio da relacao de receita e ingrediente
+    for (int i = 0; i < listaIngredientes.size(); i++) {
+      Ingrediente ingrediente = listaObjetoIngredientes.get(i);
+
+      String[] partes = listaIngredientes.get(i).split("\\(");
+      String quantidadeStr = partes[1].split("-")[0].trim();
+      String unidade = partes[1].split("-")[1].replace(")", "").trim();
+
+      Double quantidade = Double.parseDouble(quantidadeStr);
+
+      receitaDao.adicionarIngredienteNaReceita(idReceita, ingrediente.getId_ingrediente(), quantidade, unidade);
+    }
+  }
+
+  // Método para formatar o tempo de preparo
+  private int formatarTempoPreparo() {
+    String tempoPreparo = tempoDePrearoField.getText(); // Obtém o texto do campo
+    int totalSegundos = 0;
+
+    // Divide a string nos espaços para processar os elementos
+    String[] partes = tempoPreparo.split(" ");
+
+    for (int i = 0; i < partes.length; i += 2) {
+      int valor = Integer.parseInt(partes[i]); // Converte o número para inteiro
+      String unidade = partes[i + 1]; // Obtém a unidade (h, min, s)
+
+      // Converte para segundos de acordo com a unidade
+      if (unidade.equals("h")) {
+        totalSegundos += valor * 3600; // 1 hora = 3600 segundos
+      } else if (unidade.equals("min")) {
+        totalSegundos += valor * 60; // 1 minuto = 60 segundos
+      } else if (unidade.equals("s")) {
+        totalSegundos += valor; // Segundos permanecem como estão
+      }
+    }
+
+    return totalSegundos; // Retorna o tempo total em segundos
+  }
+
+  // Método para formatar o modo de preparo retornando uma string unica com todos os passos separados por quebra de linha
+  private String formatarModoPreparo() {
+    StringBuilder resultado = new StringBuilder();
+
+    for (String passosModoDePreparo : listaDePreparo.getItems()) {
+      resultado.append(passosModoDePreparo).append("\n");
+    }
+
+    return resultado.toString();
+  }
+
+  // Método para selecionar a imagem da receita
   @FXML
   void selecionar_image_receita(MouseEvent event) {
     try {
@@ -153,18 +221,12 @@ public class TelaAdicionarReceitaController implements Initializable {
 
         // converte a imagem para binário para salvar a imagem no banco de dados
         byte[] imageBytes = ImageSelector.converterImagemParaBytes(selectedFile);
-        salvarImagemNoBancoDeDados(imageBytes);
+        imagemEmBytes = imageBytes;
 
-        //Debug temporário
-        System.out.println("Imagem selecionada: " + selectedFile.getAbsolutePath());
       }
     } catch (IOException e) {
       System.out.println("Erro ao selecionar imagem: " + e.getMessage());
     }
-  }
-
-  private void salvarImagemNoBancoDeDados(byte[] imageBytes) {
-    System.out.println("Imagem salva no banco de dados!");
   }
 
   @FXML
@@ -177,49 +239,48 @@ public class TelaAdicionarReceitaController implements Initializable {
     stage.show();
   }
 
-  @FXML 
+  @FXML
   void fechar_programa(MouseEvent event) {
     System.exit(0);
   }
 
-  @FXML
-  void criar_receita(MouseEvent event) throws IOException{
-    System.out.println("Adicionando receita...");
-    System.out.println("Nome da receita: " + nomeDaReceita.getText());
-    System.out.println("Dificuldade: " + dificuldade);
-    System.out.println("Tempo de preparo: " + tempoDePreparo);
-    System.out.println("Ingredientes: ");
-    for (String ingrediente : listaIngredientes) {
-      System.out.println(ingrediente);
-    }
-    System.out.println("Modo de preparo: ");
-    for (String passo : passosModoPreparo) {
-      System.out.println(passo);
-    }
-    voltar_tela(event);
-  }
 
   @FXML
-  void adiciona_ingrediente(ActionEvent event) {
+  void adiciona_ingrediente(ActionEvent event) throws SQLException {
     String quantidade = quantidade_ingrediente.getText();
     String unidade = unidadeDeMedida.getValue();
-    String ingrediente = ingrediente_adicionado.getText();
+    String ingredienteString = ingrediente_adicionado.getText();
 
-    if (quantidade.isEmpty() || unidade == null || ingrediente.isEmpty()) {
+    if (quantidade.isEmpty() || unidade == null || ingredienteString.isEmpty()) {
       System.out.println("Preencha todos os campos!");
       return;
     }
 
-    String ingredienteFormatado = quantidade + " " + unidade + " de " + ingrediente;
-    listaIngredientes.add(ingredienteFormatado);
+    IngredienteDAO ingredienteDAO = new IngredienteDAO(ConnectionFactory.getConnection());
+    Ingrediente ingrediente = ingredienteDAO.getIngredienteByNome(ingredienteString);
 
-    ingrediente_adicionado.clear();
-    quantidade_ingrediente.clear();
-    unidadeDeMedida.setValue(null);
+    if (ingrediente == null) {
+      Alert alert = new Alert(AlertType.ERROR);
+      alert.setTitle("Erro");
+      alert.setHeaderText("Ingrediente não encontrado na nossa base de dados");
+      alert.setContentText(
+          "Por favor, verifique o nome do ingrediente e tente novamente, caso o erro persista não temos esse ingrediente em nossa base de dados");
+      alert.showAndWait();
+      return;
+    }
+
+    listaObjetoIngredientes.add(ingrediente);
+
+    String ingredienteFormatado = ingredienteString + " ( " + quantidade + " - " + unidade + " )";
+    Platform.runLater(() -> {
+      listaIngredientes.add(ingredienteFormatado);
+      ingrediente_adicionado.clear();
+      quantidade_ingrediente.clear();
+      unidadeDeMedida.setValue(null);
+    });
 
   }
 
-  // fiz com os valores 1 , 2 e 3 ja que vi que outro metodo usava essa medida
   @FXML
   void definirDificuldade(ActionEvent event) {
 
@@ -281,17 +342,6 @@ public class TelaAdicionarReceitaController implements Initializable {
       default:
         campoModoPreparo.setPromptText(proximoPasso + "º passo");
     }
-  }
-
-  @FXML
-  void adicionarTempoDePreparo(ActionEvent event) {
-    String entrada = tempoDePrearoField.getText();
-
-    int tempo = Integer.parseInt(entrada);
-
-    tempoDePreparo = tempo;
-
-    System.out.println("Tempo total de preparo: " + tempoDePreparo);
   }
 
 }
--- app/src/main/java/com/mealmatch/jdbc/dao/IngredienteDAO.java
+++ app/src/main/java/com/mealmatch/jdbc/dao/IngredienteDAO.java
@@ -29,12 +29,27 @@ public class IngredienteDAO {
     return null;
   }
 
+  //Metodo de busca ingredientes por nome utilizado no momento de adição de receita ele valida se o ingrediente já existe no banco
+  public Ingrediente getIngredienteByNome(String nome) throws SQLException {
+    String sql = "SELECT * FROM ingrediente WHERE nomeingrediente = ?";
+    try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+      stmt.setString(1, nome);
+      try (ResultSet rs = stmt.executeQuery()) {
+        if (rs.next()) {
+          return mapResultSetToIngrediente(rs);
+        }
+      }
+    }
+    return null;
+  }
+
   public Ingrediente mapResultSetToIngrediente(ResultSet rs) throws SQLException {
     Ingrediente ingrediente = new Ingrediente();
     ingrediente.setCaloria(rs.getDouble("caloria"));
     ingrediente.setCarboidrato(rs.getDouble("carboidrato"));
     ingrediente.setGordura(rs.getDouble("gordura"));
     ingrediente.setProteina(rs.getDouble("proteina"));
+    ingrediente.setNomeIngrediente(rs.getString("nomeingrediente"));
     ingrediente.setId_ingrediente(rs.getInt("idingrediente"));
     
     return ingrediente;
